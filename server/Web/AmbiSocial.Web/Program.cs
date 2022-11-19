4369b9abb55148a28a26437166747b3d43472410
emrrar <avatareigan@gmail.com>
2022-11-19 19:49:52 +0200

    File restructuring

---
+++ server/Web/AmbiSocial.Web/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", () => "Greetings!");
+
+app.Run();
