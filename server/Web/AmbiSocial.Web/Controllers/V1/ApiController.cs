ea34fc1ce3ab9eac3dc431b96c34caa98fd02ce2
enrilos <avatareigan@gmail.com>
2023-01-09 21:05:49 +0200

    Migrate to .NET 7

---
+++ server/Web/AmbiSocial.Web/Controllers/V1/ApiController.cs
@@ -0,0 +1,10 @@
+﻿namespace AmbiSocial.Web.Controllers.V1
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+    }
+}
---
+++ server/Web/AmbiSocial.Web/Controllers/V1/PostsController.cs
@@ -0,0 +1,33 @@
+﻿namespace AmbiSocial.Web.Controllers.V1
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class PostsController : ApiController
+    {
+        private readonly ILogger<PostsController> logger;
+
+        public PostsController(ILogger<PostsController> logger)
+            => this.logger = logger;
+
+
+        [HttpGet]
+        public IEnumerable<object> Get()
+        {
+            var objects = new List<object>()
+            {
+                new
+                {
+                    Origin = "Smithsonian",
+                    Value = "Considerably high"
+                },
+                new
+                {
+                    Name = "Norwegian",
+                    Value = "High"
+                }
+            };
+
+            return objects;
+        }
+    }
+}
