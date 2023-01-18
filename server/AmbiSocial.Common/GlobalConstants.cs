499bff7c0fa9bbbc601e0a09aea63fd0caf2fa63
enrilos <avatareigan@gmail.com>
2023-01-18 20:25:30 +0200

    Data skeleton

---
+++ server/AmbiSocial.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+namespace AmbiSocial.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "AmbiSocial";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ server/Data/AmbiSocial.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace AmbiSocial.Data.Models
+{
+    using AmbiSocial.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string? Name { get; set; }
+
+        public string? Value { get; set; }
+    }
+}
