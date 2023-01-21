3cc9a8612e6a524291a4188a6e56b576dc5eb6a0
enrilos <avatareigan@gmail.com>
2023-01-21 13:09:20 +0200

    Use Npgsql

--- server/Data/AmbiSocial.Data/DesignTimeDbContextFactory.cs
+++ server/Data/AmbiSocial.Data/DesignTimeDbContextFactory.cs
@@ -10,13 +10,19 @@
     {
         public ApplicationDbContext CreateDbContext(string[] args)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            var connectionString = Environment.GetEnvironmentVariable("DefaultDockerConnection");
+
+            if (connectionString == null)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                connectionString = configuration.GetConnectionString("DefaultConnection");
+            }
+
             builder.UseNpgsql(connectionString);
 
             return new ApplicationDbContext(builder.Options);
--- server/Web/AmbiSocial.Web/Program.cs
+++ server/Web/AmbiSocial.Web/Program.cs
@@ -1,16 +1,31 @@
+using AmbiSocial.Data;
+using AmbiSocial.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options =>
+        options.UseNpgsql(Environment.GetEnvironmentVariable("DefaultDockerConnection") ?? builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services
+    .AddDefaultIdentity<ApplicationUser>(IdentityOptionsProvider.GetIdentityOptions)
+    .AddRoles<ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
 var app = builder.Build();
 
+using (var serviceScope = app.Services.CreateScope())
+{
+    var dbContext = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
-app.MapGet("/", () => "Greetings!");
 
 app.Run();
