194f69d7cd237088ada1154cc726524ee1acefd1
enrilos <avatareigan@gmail.com>
2023-02-11 15:31:37 +0200

    Add useQueryParam hook

---
+++ client/src/utils/navigation.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+export function useQueryParam(name: string) {
+    const location = useLocation();
+    const [value, setValue] = useState(new URLSearchParams(location.search).get(name));
+
+    useEffect(() => {
+        setValue(new URLSearchParams(location.search).get(name));
+    }, [location]);
+
+    return value;
+}
